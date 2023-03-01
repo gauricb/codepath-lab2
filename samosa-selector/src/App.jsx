137766554dd07073d64f2f53b9b9862c8608c5e6
gauricb <gbarar@umass.edu>
2023-03-01 15:34:04 -0500

    added hooks

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,7 +1,63 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  return <div className="App"></div>;
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+    }
+  };
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  };
+
+  const buyFullFeast = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 10);
+      setCount(count - 100);
+    }
+  };
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => setCount(count + multiplier);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count}</h2>
+        <img
+          className="samosa"
+          src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png"
+          onClick={updateCount}
+        />
+      </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Party Pack</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 samosas</button>
+        </div>
+
+        <div className="upgrade">
+          <h3>Full Feast</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 samosas</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
